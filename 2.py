74c060e03b113d2d77c2d3f728061047db55541c
nicolesambile <nicolesambile026@gmail.com>
2025-11-18 13:07:32 +0800

    update

--- 2.py
+++ 2.py
@@ -1,3 +1,5 @@
 l_nbs = float(input("Enter length l: "))
-w_nbs = float(input("Enter length w: "))
-p_nbs = 2 *(l_nbs + w_nbs)
+w_nbs = float(input("Enter width w: "))
+
+p_nbs = 2 * (l_nbs + w_nbs)
+print("The perimeter of the rectangle is:", p_nbs)
